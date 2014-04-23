5230cae59d9dd9593775ecd9401e117e08c61dd9
Rafael Cortês <mrfyda@gmail.com>
2014-04-23 10:58:14 +0100

    new: translator

--- proj2/g17/src/ist/meic/pa/TraceVM.java
+++ proj2/g17/src/ist/meic/pa/TraceVM.java
@@ -1,9 +1,35 @@
 package ist.meic.pa;
 
+import javassist.*;
+
 public class TraceVM {
 
     public static void main(String[] args) {
-        // TODO: do something here
+        if (args.length < 1) {
+            System.err.println("usage: ist.meic.pa.TraceVM <target application class name> [<target application arguments>]");
+            System.exit(1);
+        }
+
+        Translator translator = new TracerTranslator();
+        ClassPool pool = ClassPool.getDefault();
+        Loader classLoader = new Loader();
+
+        try {
+            classLoader.addTranslator(pool, translator);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        } catch (CannotCompileException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            String[] restArgs = new String[args.length - 1];
+            System.arraycopy(args, 1, restArgs, 0, restArgs.length);
+
+            classLoader.run(args[0], restArgs);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
 }
