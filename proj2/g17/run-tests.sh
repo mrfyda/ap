35503fba3a5db88a4c48c91d56d5cc47395e0af4
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-05-05 17:03:51 +0100

    test runner

---
+++ proj2/g17/run-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -rf workdir;
+mkdir -p workdir;
+cp lib/javassist.jar workdir/;
+cp tracer.jar workdir/;
+
+for TEST in $(ls tests)
+do
+  echo "Executing $TEST ...";
+  cp tests/$TEST/$TEST.java workdir/;
+  cp tests/$TEST/$TEST.out workdir/;
+  cd workdir;
+	javac -cp .:javassist.jar:tracer.jar $TEST.java > /dev/null 2>&1;
+  java -cp .:javassist.jar:tracer.jar ist.meic.pa.TraceVM $TEST >& $TEST.created;
+  DIFF=$(diff $TEST.out $TEST.created);
+
+  if [ -n "$DIFF" ]; then
+    echo "[FAILED] $TEST";
+  else
+    echo "[OK] $TEST";
+  fi
+
+  cd ..;
+  echo "--------------------";
+done
+
+rm -rf workdir;
