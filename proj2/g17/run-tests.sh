000cc4fd9abb1be141fea7cbe36e527636f02cc0
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-05-07 09:57:57 +0100

    indent bash script

--- proj2/g17/run-tests.sh
+++ proj2/g17/run-tests.sh
@@ -5,24 +5,26 @@ mkdir -p workdir;
 cp lib/javassist.jar workdir/;
 cp tracer.jar workdir/;
 
-for TEST in $(ls tests)
-do
-  echo "Executing $TEST ...";
-  cp tests/$TEST/$TEST.java workdir/;
-  cp tests/$TEST/$TEST.out workdir/;
-  cd workdir;
-	javac -cp .:javassist.jar:tracer.jar $TEST.java > /dev/null 2>&1;
-  java -cp .:javassist.jar:tracer.jar ist.meic.pa.TraceVM $TEST >& $TEST.created;
-  DIFF=$(diff $TEST.out $TEST.created);
+for TEST in $(ls tests); do
+    echo "Executing $TEST ...";
 
-  if [ -n "$DIFF" ]; then
-    echo "[FAILED] $TEST";
-  else
-    echo "[OK] $TEST";
-  fi
+    cp tests/$TEST/$TEST.java workdir/;
+    cp tests/$TEST/$TEST.out workdir/;
+    cd workdir;
 
-  cd ..;
-  echo "--------------------";
+    javac -cp .:javassist.jar:tracer.jar $TEST.java > /dev/null 2>&1;
+
+    java -cp .:javassist.jar:tracer.jar ist.meic.pa.TraceVM $TEST >& $TEST.created;
+
+    DIFF=$(diff $TEST.out $TEST.created);
+    if [ -n "$DIFF" ]; then
+        echo "[FAILED] $TEST";
+    else
+        echo "[OK] $TEST";
+    fi
+
+    cd ..;
+    echo "-------------------";
 done
 
 rm -rf workdir;
